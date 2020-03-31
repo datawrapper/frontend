edb23dd2993edce09c7cb4623cc52bd94165d220
Gregor Aisch <gka@users.noreply.github.com>
2020-03-31 15:31:54 +0200

    preload locales on server start

--- src/routes/preview/[chartId]/locale-[locale].json.js
+++ src/routes/preview/[chartId]/locale-[locale].json.js
@@ -1,38 +1,53 @@
 import path from 'path';
 import fs from 'fs-extra';
 
+const VENDORS = ['dayjs', 'numeral'];
+const LOCALES = {};
+
 export async function get(req, res, next) {
     const { locale } = req.params;
 
-    const locales = {
-        dayjs: await loadVendorLocale('dayjs', locale),
-        numeral: await loadVendorLocale('numeral', locale)
-    };
+    const locales = {};
+    for (var i = 0; i < VENDORS.length; i++) {
+        locales[VENDORS[i]] = await loadVendorLocale(VENDORS[i], locale);
+    }
 
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(locales));
 }
 
 async function loadVendorLocale(vendor, locale) {
-    const basePath = path.resolve(
-        __dirname,
-        '../../../node_modules/@datawrapper/locales/locales/',
-        vendor
-    );
     const culture = locale.replace('_', '-').toLowerCase();
-    const tryFiles = [`${culture}.js`];
+    const tryLocales = [culture];
     if (culture.length > 2) {
         // also try just language as fallback
-        tryFiles.push(`${culture.substr(0, 2)}.js`);
+        tryLocales.push(culture.split('-')[0]);
     }
-    for (let i = 0; i < tryFiles.length; i++) {
-        const file = path.join(basePath, tryFiles[i]);
-        try {
-            return await fs.readFile(file, 'utf-8');
-        } catch (e) {
-            // file not found, so try next
+    for (let i = 0; i < tryLocales.length; i++) {
+        if (LOCALES[vendor].has(tryLocales[i])) {
+            return LOCALES[vendor].get(tryLocales[i]);
         }
     }
     // no locale found at all
     return 'null';
+}
+
+export async function preloadLocales() {
+    for (var i = 0; i < VENDORS.length; i++) {
+        const vendor = VENDORS[i];
+        LOCALES[vendor] = new Map();
+        const basePath = path.resolve(
+            __dirname,
+            '../../../node_modules/@datawrapper/locales/locales/',
+            vendor
+        );
+        const files = await fs.readdir(basePath);
+        for (let i = files.length - 1; i >= 0; i--) {
+            const file = files[i];
+            if (/.*\.js/.test(file)) {
+                const content = await fs.readFile(path.join(basePath, file), 'utf-8')
+                LOCALES[vendor].set(path.basename(file, '.js'), content);
+            }
+        }
+    }
 }
