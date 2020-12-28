a3b7548fab58c6f66c9bf6a9c3eac607166f798a
David Kokkelink <david@datawrapper.de>
2020-12-28 22:17:53 +0100

    adapt to new chart data structure

--- src/routes/preview/index.js
+++ src/routes/preview/index.js
@@ -9,7 +9,6 @@ module.exports = {
     version: '1.0.0',
     register: async (server, options) => {
         const { loadLocales, loadVendorLocale, createAPI, initCaches } = require('./utils');
-        const { getStyles, getVis, getTheme } = initCaches(server);
         const locales = await loadLocales();
         const config = server.methods.config();
         const apiBase = `${config.api.https ? 'https' : 'http'}://${config.api.subdomain}.${config.api.domain
@@ -32,77 +31,39 @@ module.exports = {
 
                 const queryString = Object.entries({
                     published: request.query.published,
-                    ott: request.query.ott
+                    ott: request.query.ott,
+                    theme: request.query.theme
                 })
                     .filter(([, value]) => Boolean(value))
                     .map(([key, value]) => `${key}=${value}`)
                     .join('&');
 
 
-                let publishData, vis, theme, css, csv;
+                let props;
 
                 try {
-                    publishData = await api(`/charts/${chartId}/publish/data?${queryString}`);
-                    chart = publishData.chart;
-                    csv = publishData.data;
-                    delete publishData.data;
-                    delete publishData.chart;
+                    props = await api(`/charts/${chartId}/publish/data?${queryString}`);
                 } catch (ex) {
                     return Boom.unauthorized();
                 }
 
-                const themeName = request.query.theme || chart.theme;
-
-                try {
-                    const results = await Promise.all([
-                        getVis(api, chart.type),
-                        getTheme(api, themeName),
-                        getStyles(api, chart.type, themeName, !!request.query.transparent)
-                    ]);
-
-                    vis = results[0];
-                    theme = results[1];
-                    css = results[2];
-
-                    theme.less = '';
-                    vis.locale = publishData.locales;
-                    delete publishData.locales;
-                } catch (error) {
-                    server.logger.error(`Error fetching information for ${chart.id}: ${error.message}`);
-                    return Boom.badImplementation();
-                }
-
-                const chartLocale = chart.language || 'en-US';
+                const chartLocale = props.chart.language || 'en-US';
 
                 const deps = getDependencies({
-                    locale: chartLocale,
-                    dependencies: vis.dependencies
+                    locale: props.chart.language,
+                    dependencies: props.visualization.dependencies
                 });
 
-                const libraries = vis.libraries.map(lib => lib.uri);
-
-                const props = {
-                    data: {
-                        visJSON: vis,
-                        chartJSON: chart,
-                        publishData,
-                        chartData: csv,
-                        isPreview: true,
-                        chartLocale,
-                        locales: {
-                            dayjs: loadVendorLocale(locales, 'dayjs', chartLocale),
-                            numeral: loadVendorLocale(locales, 'numeral', chartLocale)
-                        },
-                        metricPrefix: {} /* NOTE: What about this? */,
-                        themeId: theme.id,
-                        fontsJSON: theme.fonts,
-                        typographyJSON: theme.data.typography,
-                        polyfillUri: `/lib/polyfills`
+                props = Object.assign(props, {
+                    isIframe: true,
+                    isPreview: true,
+                    locales: {
+                        dayjs: loadVendorLocale(locales, 'dayjs', chartLocale),
+                        numeral: loadVendorLocale(locales, 'numeral', chartLocale)
                     },
-                    theme,
-                    translations: vis.locale
-                };
+                });
 
+                const libraries = props.visualization.libraries.map(lib => lib.uri);
                 const { html, head } = chartCore.svelte.render(props);
 
                 return h.view('preview', {
@@ -113,16 +74,16 @@ module.exports = {
                     }),
                     CHART_HTML: html,
                     CHART_HEAD: head,
-                    VIS_SCRIPT: `${apiBase}/visualizations/${props.data.visJSON.id}/script.js`,
+                    VIS_SCRIPT: `${apiBase}/visualizations/${props.visualization.id}/script.js`,
                     MAIN_SCRIPT: '/lib/chart-core/main.js',
                     POLYFILL_SCRIPT: '/lib/chart-core/load-polyfills.js',
                     DEPS: deps.map(el => `/lib/chart-core/${el}`),
                     LIBRARIES: libraries,
-                    CSS: css,
+                    CSS: props.styles,
                     CHART_CLASS: [
-                        `vis-height-${get(vis, 'height', 'fit')}`,
-                        `theme-${get(theme, 'id')}`,
-                        `vis-${get(vis, 'id')}`
+                        `vis-height-${get(props.visualization, 'height', 'fit')}`,
+                        `theme-${get(props.theme, 'id')}`,
+                        `vis-${get(props.visualization, 'id')}`
                     ]
                 });
             }
