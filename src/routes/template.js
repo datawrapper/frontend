548817fdc3b5f2284e79428e5f1585f4c395bf09
Gregor Aisch <gka@users.noreply.github.com>
2021-01-05 01:33:17 +0100

    add Svelte view to frontend, use for template route

--- src/routes/template.js
+++ src/routes/template.js
@@ -1,6 +1,7 @@
 const Boom = require('@hapi/boom');
 const jsesc = require('jsesc');
 
+
 module.exports = {
     name: 'routes/template',
     version: '1.0.0',
@@ -32,43 +33,10 @@ module.exports = {
                 const { config } = request.server.methods;
                 const api = config('api');
                 const { domain, subdomain } = api;
-                return h.view('template', {
-                    LIBS: [
-                        '/static/vendor/requirejs/require.js',
-                        '/static/vendor/jquery/jquery.min.js',
-                        '/static/vendor/underscore/underscore-min.js',
-                        '/lib/chart-core/dw-2.0.min.js'
-                    ],
-                    API_DOMAIN: `${subdomain}.${domain}`,
-                    TEMPLATE_HTML: `
-    <script>
-        require(['dw/backend/httpReq'], function(httpReq) {
-            var chartData = JSON.parse(${jsesc(JSON.stringify(chartData), {
-                            isScriptContext: true,
-                            json: true,
-                            wrap: true
-                        })});
-            window.openInDatawrapper = function() {
-                httpReq.post('/v3/charts', {
-                    payload: chartData
-                }).then(res => {
-                    // upload data
-                    httpReq.put('/v3/charts/'+res.id+'/data', {
-                        headers: {
-                            'Content-Type': 'text/csv'
-                        },
-                        data: ${JSON.stringify(dataset)}
-                    }).then(res2 => {
-                        // redirect to chart
-                        window.location.href = res.type === 'locator-maps' ? '/edit/'+res.id : '/chart/'+res.id+'/edit';
-                    });
-                });
-            }
-        })
-    </script>
-    <button onclick="openInDatawrapper()">Edit this chart in Datawrappper</button>
-`,
-                });;
+
+                const props = { chartData, dataset, apiDomain: `${subdomain}.${domain}` };
+                const layout = 'SignInPageLayout';
+                return h.view('EditInDatawrapper.svelte', { layout, props });
             }
         });
     }
