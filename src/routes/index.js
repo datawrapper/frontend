396f23c83cacd34f8b22cd5407891ed275b5a792
Gregor Aisch <gka@users.noreply.github.com>
2021-04-13 08:38:26 +0200

    drop /v2 from /create route

--- src/routes/index.js
+++ src/routes/index.js
@@ -30,7 +30,7 @@ module.exports = {
 
         await server.register(require('./create'), {
             routes: {
-                prefix: '/v2/create'
+                prefix: '/create'
             }
         });
     }
