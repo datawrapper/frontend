fbc678d2a1bfb9577f8617c36115edf2a68111eb
Gregor Aisch <gka@users.noreply.github.com>
2021-04-27 10:50:51 +0200

    feat: svelte navbar with plugin support

--- src/views/layout/partials/PageHeader.svelte
+++ src/views/layout/partials/PageHeader.svelte
@@ -4,20 +4,35 @@
     import NavBar from './header/NavBar.svelte';
 
     const request = getContext('request');
+    let isActive = false;
 </script>
 
 <header class="py-5 mb-5" id="top">
     <div class="container">
-        <div class="flex">
-            {#if $request.path === '/'}
-                <DatawrapperLogo height="40" />
-            {:else}
-                <a href="/" style="line-height: 1">
+        <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                {#if $request.path === '/'}
                     <DatawrapperLogo height="40" />
+                {:else}
+                    <a class="navbar-item" href="/" style="line-height: 1">
+                        <DatawrapperLogo height="40" />
+                    </a>
+                {/if}
+
+                <a
+                    role="button"
+                    class:is-active={isActive}
+                    class="navbar-burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    on:click={() => (isActive = !isActive)}>
+                    <span aria-hidden="true" />
+                    <span aria-hidden="true" />
+                    <span aria-hidden="true" />
                 </a>
-            {/if}
-            <NavBar />
-        </div>
+            </div>
+            <NavBar {isActive} />
+        </nav>
     </div>
 </header>
 
@@ -25,8 +40,5 @@
     header {
         /*background: white;
         border-bottom: 1px solid #eee;*/
-    }
-    .container {
-        margin-bottom: 0;
     }
 </style>
--- src/views/layout/partials/header/NavBar.svelte
+++ src/views/layout/partials/header/NavBar.svelte
@@ -1,14 +1,71 @@
 <script>
     import { getContext } from 'svelte';
+    import SvgIcon from 'layout/partials/SvgIcon.svelte';
+
+    export let isActive;
+
     const user = getContext('user');
+    const config = getContext('config');
     const request = getContext('request');
 </script>
 
-<nav>
-    <ul />
-</nav>
+<div class="navbar-menu" class:is-active={isActive}>
+    <div class="navbar-end">
+        {#each $config.headerLinks as link}
+            {#if link.id === 'separator'}
+                <div class="navbar-separator mx-3" />
+            {:else if link.submenuItems}
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link is-arrowless"
+                        >{#if link.svgIcon}<SvgIcon
+                                size="1.2rem"
+                                valign="top"
+                                icon={link.svgIcon}
+                            />{/if}{#if link.fontIcon}<span class="icon"
+                                ><i class={link.fontIcon} /></span
+                            >{/if} <span>{@html link.title || ''}</span></a
+                    >
+                    <div class="navbar-dropdown is-right">
+                        {#each link.submenuItems as subItem}
+                            {#if subItem.id === 'separator'}
+                                <hr class="navbar-divider" />
+                            {:else}
+                                <a class="navbar-item" href={subItem.url}
+                                    >{#if subItem.svgIcon}<SvgIcon
+                                            size="1rem"
+                                            valign="top"
+                                            icon={subItem.svgIcon}
+                                        />{/if}{#if subItem.fontIcon}<span class="icon"
+                                            ><i class={subItem.fontIcon} /></span
+                                        >{/if} <span>{@html subItem.title || ''}</span></a
+                                >
+                            {/if}
+                        {/each}
+                    </div>
+                </div>
+            {:else}
+                <a
+                    class:is-active={$request.path.startsWith(link.url)}
+                    class="navbar-item"
+                    href={link.url}
+                    >{#if link.svgIcon}<SvgIcon
+                            size="1.2rem"
+                            valign="top"
+                            icon={link.svgIcon}
+                        />{/if}{#if link.fontIcon}<span class="icon"
+                            ><i class={link.fontIcon} /></span
+                        >{/if} <span>{@html link.title || ''}</span></a
+                >
+            {/if}
+        {/each}
+    </div>
+</div>
 
 <style>
+    .navbar-separator {
+        border-left: 1px solid #d7d7d7;
+        border-right: 1px solid #ffffff;
+    }
     nav ul {
         margin: 0;
         display: flex;
