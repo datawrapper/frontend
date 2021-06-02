8453309bf26f6dd079a088b873ab17b54e9f7853
Gregor Aisch <gka@users.noreply.github.com>
2021-06-02 10:33:05 +0200

    store Joi reference in server.app.Joi so plugins can use it

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ const { requireConfig } = require('@datawrapper/service-utils/findConfig');
 const registerVisualizations = require('@datawrapper/service-utils/registerVisualizations');
 const config = requireConfig();
 const path = require('path');
+const Joi = require('joi');
 const { getUserLanguage } = require('./utils');
 const {
     SvelteView,
@@ -142,6 +143,7 @@ const start = async () => {
     // hooks
     server.app.event = eventList;
     server.app.events = new FrontendEventEmitter({ logger: server.logger, eventList });
+    server.app.Joi = Joi;
 
     server.views({
         engines: {
