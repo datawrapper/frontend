fc6d8913844c89dded518d0e2d161e383aef5333
Gregor Aisch <gka@users.noreply.github.com>
2021-08-22 17:11:48 +0200

    visualize step mockup

--- src/views/edit/Visualize.svelte
+++ src/views/edit/Visualize.svelte
@@ -1,1 +1,38 @@
-<div class="container">Visualize</div>
+<script>
+    import Tabs from 'layout/partials/bulma/Tabs.svelte';
+    export let chart;
+
+    let active = 'refine';
+</script>
+
+<div class="container">
+    <div class="columns">
+        <div class="column is-one-third">
+            <Tabs
+                items={[
+                    { id: 'vis', title: 'Chart type' },
+                    { id: 'refine', title: 'Refine' },
+                    { id: 'annotate', title: 'Annotate' },
+                    { id: 'design', title: 'Design' }
+                ]}
+                bind:active
+            />
+
+            {#if active === 'annotate'}
+                Title: <input type="text" class="input" bind:value={$chart.title} />
+                Description:
+                <input type="text" class="input" bind:value={$chart.metadata.describe.intro} />
+            {/if}
+        </div>
+        <div class="column">
+            <div class="box">chart preview</div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .box {
+        margin: 0 auto;
+        width: 550px;
+    }
+</style>
