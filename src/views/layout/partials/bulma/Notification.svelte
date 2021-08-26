7013ea93168bd15bad89d91ad252e7eb40e68cfe
Gregor Aisch <gka@users.noreply.github.com>
2021-08-26 11:01:23 +0000

    feat: show list of teams in submenu (#87)
    * feat: show list of teams in submenu
    
    * feat: show active team in create menu
    
    * styles refactoring to reach TeamSelect and VisArchive
    
    * feat: user can switch active team via submenu
    
    * ui: fix styles in vis archive
    
    * show active team in hello world view
    
    * check if user.teams exists
    
    * show active team in hello world page
    
    * add "(private)" to no team
    
    * bump service-utils

--- src/views/layout/partials/bulma/Notification.svelte
+++ src/views/layout/partials/bulma/Notification.svelte
@@ -2,10 +2,11 @@
     export let deletable = true;
     export let type = '';
     export let visible = true;
+    export let className = '';
 </script>
 
 {#if visible}
-    <div class="notification {type ? `is-${type}` : ''}">
+    <div class="notification {type ? `is-${type}` : ''} {className}">
         {#if deletable}<button
                 aria-label="delete"
                 on:click={() => (visible = false)}
