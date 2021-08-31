8db3383a53dcb9b099098b8e73a415235604d051
David Wendler <davidw-dw@users.noreply.github.com>
2021-09-01 00:35:34 +0200

    test alternative markup/classes w/o custom css

--- src/views/layout/SignInPageLayout.svelte
+++ src/views/layout/SignInPageLayout.svelte
@@ -15,56 +15,28 @@
     <title>Datawrapper{title ? ` - ${title}` : ''}</title>
 </svelte:head>
 
-<div class="container">
-    <div class="columns is-vcentered ">
-        <div class="column is-1" />
-        <div class="column">
-            <div class="columns is-gapless">
-                <div class="column box left">
-                    <DatawrapperLogo height="60" />
-
-                    <div class="terms is-size-7">
-                        {@html __('signin / terms')}
-                    </div>
-                </div>
-                <div class="column is-8 box right">
-                    <slot />
+<section class="hero is-fullheight">
+    <div class="hero-body is-justify-content-center">
+        <div class="box columns is-radiusless p-0">
+            <div
+                class="column p-6 is-flex is-flex-direction-column is-justify-content-space-between"
+            >
+                <DatawrapperLogo height="60" />
+                <div class="terms is-size-7">
+                    {@html __('signin / terms')}
                 </div>
             </div>
+            <div class="column is-8 p-6 has-background-light">
+                <slot />
+            </div>
         </div>
-        <div class="column is-1" />
     </div>
-</div>
+</section>
 
 <style>
     :global(html) {
         background-color: #207693;
         background-image: url(/lib/static/img/signin-background.png);
-    }
-    .columns.is-vcentered {
-        min-height: 100vh;
-    }
-
-    .terms {
-        position: absolute;
-        bottom: 0px;
-        left: 0;
-        padding: 50px 90px 50px 50px;
-    }
-    .box {
-        box-shadow: none;
-    }
-    .box.left {
-        border-top-right-radius: 0;
-        border-bottom-right-radius: 0;
-        padding: 50px !important;
-        position: relative;
-    }
-    .box.right {
-        border-top-left-radius: 0;
-        border-bottom-left-radius: 0;
-        background: var(--color-dw-background);
-        padding: 50px 100px !important;
     }
     @media screen and (min-height: 600px) and (min-width: 600px) {
         :global(html) {
