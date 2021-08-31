32baf8dc81fbe76cd32c2ff1fc0ad37bae9fc307
Gregor Aisch <gka@users.noreply.github.com>
2021-08-31 08:44:12 +0200

    feat: new signup form design

--- src/views/layout/partials/SignUpForm.svelte
+++ src/views/layout/partials/SignUpForm.svelte
@@ -7,7 +7,7 @@
     }
 
     let step = 'signup'; // can also be 'signin', 'password-reset', or 'otp'
-    const providers = ['facebook', 'google', 'github'];
+    const providers = ['google', 'twitter', 'facebook', 'github'];
 
     let emailOpen = false;
     let passwordClear = false;
@@ -29,59 +29,79 @@
             <p>{@html __('login / signup / intro')}</p>
 
             {#if emailOpen}
-                <div class="field">
-                    <label for="su-email" class="label">Email</label>
-                    <input id="su-email" placeholder="name@domain.tld" class="input" type="email" />
-                </div>
-                <div class="field">
-                    <label for="su-pwd" class="label">Password</label>
-                    <input id="su-pwd" class="input" type={passwordClear ? 'text' : 'password'} />
-                </div>
-                <div class="field">
-                    <label class="checkbox"
-                        ><input bind:checked={passwordClear} type="checkbox" />
-                        {@html __('account / invite / password-clear-text')}</label
-                    >
-                </div>
+                <div class="signup-form">
+                    <div class="field">
+                        <label for="su-email" class="label">Email</label>
+                        <input
+                            id="su-email"
+                            placeholder="name@domain.tld"
+                            class="input"
+                            type="email"
+                        />
+                    </div>
+                    <div class="field">
+                        <label for="su-pwd" class="label">Password</label>
+                        <input
+                            id="su-pwd"
+                            class="input"
+                            type={passwordClear ? 'text' : 'password'}
+                        />
+                    </div>
+                    <div class="field">
+                        <label class="checkbox"
+                            ><input bind:checked={passwordClear} type="checkbox" />
+                            {@html __('account / invite / password-clear-text')}</label
+                        >
+                    </div>
 
-                <button
-                    class="button is-primary is-fullwidth mb-2"
-                    on:click={() => (emailOpen = true)}
-                >
-                    <span class="icon mr-1">
-                        <i class="fa fa-pencil" />
-                    </span>
-                    {@html __('Sign Up')}</button
-                >
-                <button class="button is-fullwidth mb-2" on:click={() => (emailOpen = false)}>
-                    Use different provider</button
-                >
-            {:else}
-                <button class="button is-fullwidth mb-2" on:click={() => (emailOpen = true)}>
-                    <span class="icon mr-1">
-                        <i class="fa fa-email" />
-                    </span>
-                    Sign up using email</button
-                >
-                <p class="has-text-grey has-text-centered">⁓⁓⁓ or ⁓⁓⁓</p>
-                {#each providers as provider}
-                    <a href="/signin/{provider}" class="button is-fullwidth mb-2">
-                        <span class="icon mr-1">
-                            <i class="fa fa-{provider}" />
-                        </span>
-                        Sign in using {capitalize(provider)}</a
+                    <button class="button is-primary mb-2" on:click={() => (emailOpen = true)}>
+                        {@html __('Sign Up')}</button
                     >
-                {/each}
+                    <div class="mt-5">
+                        <a href="#/back" on:click|preventDefault={() => (emailOpen = false)}>
+                            ←&nbsp;&nbsp;Choose a different provider</a
+                        >
+                    </div>
+                </div>
+            {:else}
+                <div class="signup-form">
+                    <button
+                        class="button provider-email is-fullwidth mb-2"
+                        on:click={() => (emailOpen = true)}
+                    >
+                        <span class="icon mr-1">
+                            <i class="fa fa-envelope" />
+                        </span>
+                        Sign up using email</button
+                    >
+                </div>
+                <hr />
+                <div class="signup-form">
+                    {#each providers as provider}
+                        <a
+                            href="/signin/{provider}"
+                            class="button provider-{provider} is-fullwidth mb-2"
+                        >
+                            <span class="icon mr-1">
+                                <i class="fa fa-{provider}" />
+                            </span>
+                            Sign up using {capitalize(provider)}</a
+                        >
+                    {/each}
+                </div>
             {/if}
-            <hr class="mt-4 mb-2" />
-            <p class="has-text-grey mt-3">
-                Already have an account? <a
+
+            <hr />
+            <p class=" mt-3">
+                <strong>Already have an account?</strong><br />
+                <a
                     href="#/signin"
+                    class="has-text-weight-bold"
                     on:click|preventDefault={() => {
                         step = 'signin';
                         emailOpen = false;
-                    }}>Click here to sign in</a
-                >
+                    }}>Sign in here</a
+                >.
             </p>
         </div>
     {:else if step === 'signin'}
@@ -89,56 +109,119 @@
             <h2 class="title is-3">{@html __('login / login / headline')}</h2>
             <p>{@html __('login / login / intro')}</p>
             {#if emailOpen}
-                <div class="field">
-                    <label for="si-email" class="label">Email</label>
-                    <input id="si-email" placeholder="name@domain.tld" class="input" type="email" />
-                </div>
-                <div class="field">
-                    <label for="si-pwd" class="label">Password</label>
-                    <input id="si-pwd" class="input" type="password" />
-                </div>
-                <button
-                    class="button is-primary is-fullwidth mb-2"
-                    on:click={() => (emailOpen = true)}
-                >
-                    <span class="icon mr-1">
-                        <i class="fa fa-sign-in" />
-                    </span>
-                    {@html __('Login')}</button
-                >
-                <button class="button is-fullwidth mb-2" on:click={() => (emailOpen = false)}>
-                    Log in using different provider</button
-                >
-            {:else}
-                <button class="button is-fullwidth mb-2" on:click={() => (emailOpen = true)}>
-                    <span class="icon mr-1">
-                        <i class="fa fa-email" />
-                    </span>
-                    Log in using email</button
-                >
-                <p class="has-text-grey has-text-centered">⁓⁓⁓ or ⁓⁓⁓</p>
-                {#each providers as provider}
-                    <a href="/signin/{provider}" class="button is-fullwidth mb-2">
-                        <span class="icon mr-1">
-                            <i class="fa fa-{provider}" />
-                        </span>
-                        Sign in using {capitalize(provider)}</a
+                <div class="signup-form">
+                    <div class="field">
+                        <label for="si-email" class="label">Email</label>
+                        <input
+                            id="si-email"
+                            placeholder="name@domain.tld"
+                            class="input"
+                            type="email"
+                        />
+                    </div>
+                    <div class="field">
+                        <label for="si-pwd" class="label">Password</label>
+                        <input id="si-pwd" class="input" type="password" />
+                    </div>
+                    <button class="button is-primary mb-2" on:click={() => (emailOpen = true)}>
+                        {@html __('Login')}</button
                     >
-                {/each}
+                    <div class="mt-5">
+                        <a href="#/back" on:click|preventDefault={() => (emailOpen = false)}>
+                            ←&nbsp;&nbsp;Choose a different provider</a
+                        >
+                    </div>
+                </div>
+            {:else}
+                <div class="signup-form">
+                    <button
+                        class="button provider-email is-fullwidth mb-2"
+                        on:click={() => (emailOpen = true)}
+                    >
+                        <span class="icon mr-1">
+                            <i class="fa fa-envelope" />
+                        </span>
+                        Sign in using email</button
+                    >
+                </div>
+                <hr />
+                <div class="signup-form">
+                    {#each providers as provider}
+                        <a
+                            href="/signin/{provider}"
+                            class="provider-{provider} button is-fullwidth mb-2"
+                        >
+                            <span class="icon mr-1">
+                                <i class="fa fa-{provider}" />
+                            </span>
+                            Sign in with {capitalize(provider)}</a
+                        >
+                    {/each}
+                </div>
             {/if}
 
-            <hr class="mt-4 mb-2" />
-            <p class="has-text-grey mt-3">
-                Don't have an account, yet? <a
+            <hr />
+            <p class="mt-3">
+                <strong>Don't have an account, yet?</strong><br />
+                <a
                     href="#/signin"
+                    class="has-text-weight-bold"
                     on:click|preventDefault={() => {
                         step = 'signup';
                         emailOpen = false;
-                    }}>Click here to create one (for free)</a
-                >
+                    }}>Create a new account</a
+                >. It's free and done quickly.
             </p>
         </div>
     {:else if step === 'otp'}
         <p>Please enter your one time password token:</p>
     {/if}
 </div>
+
+<style lang="less">
+    @google: #EA4335;
+    @facebook: #1877F2;
+    @twitter: #1D9BF0;
+
+    .button.is-fullwidth {
+        text-align: left;
+        justify-content: left;
+        padding-left: 70px;
+    }
+
+    .signup-form {
+        max-width: 300px;
+
+        .button .icon {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 50px;
+            height: 100%;
+            border-top-left-radius: 4px;
+            border-bottom-left-radius: 4px;
+            background: fade(#333333, 10%);
+            color: #333333;
+            font-size: 24px;
+            margin-left: 0;
+
+            i {
+                position: relative;
+                top: 1px;
+            }
+        }
+
+        :global(.button.provider-facebook .icon) {
+            background: fade(@facebook, 10%);
+            color: @facebook;
+        }
+        :global(.button.provider-google .icon) {
+            background: fade(@google, 10%);
+            color: @google;
+        }
+        :global(.button.provider-twitter .icon) {
+            background: fade(@twitter, 10%);
+            color: @twitter;
+        }
+    }
+</style>
