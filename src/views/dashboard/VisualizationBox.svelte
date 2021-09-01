a423de5398f2fb1fdfa0d22fda088278d0c1fba0
Gregor Aisch <gka@users.noreply.github.com>
2021-09-01 16:31:27 +0200

    refactoring
    show thumbnails

---
+++ src/views/dashboard/VisualizationBox.svelte
@@ -0,0 +1,24 @@
+<script>
+    import purifyHTML from '@datawrapper/shared/purifyHtml';
+    export let chart;
+</script>
+
+<figure class="box has-border p-2 pb-1 image is-4by3">
+    <a href="/chart/{chart.id}/edit">
+        <div class="title is-6 mb-2">
+            {purifyHTML(chart.title, '')}
+        </div>
+        <img
+            alt="preview"
+            src="//charts.datawrapper.local/preview/{chart.id}/{chart.thumbnailHash}/plain.png"
+        /></a
+    >
+</figure>
+
+<style>
+    .title {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+</style>
