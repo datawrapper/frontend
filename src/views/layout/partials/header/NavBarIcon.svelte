02bdec9f9d04e803fd1011a9952bd654ae8650f0
Gregor Aisch <gka@users.noreply.github.com>
2021-09-02 10:20:25 +0200

    wrap all navbar icons in NavBarIcon component

---
+++ src/views/layout/partials/header/NavBarIcon.svelte
@@ -0,0 +1,19 @@
+<script>
+    import SvgIcon from 'layout/partials/SvgIcon.svelte';
+    export let item;
+</script>
+
+{#if item.svgIcon}<SvgIcon
+        size={item.svgIconSize || '20px'}
+        crisp={!!item.svgIconCrisp}
+        valign="top"
+        icon={item.svgIcon}
+        className="{item.title ? '' : 'mr-0'} {item.svgIconClass || ''}"
+    />{/if}{#if item.fontIcon}<span class="icon"><i class={item.fontIcon} /></span>{/if}
+
+<style>
+    :global(.navbar-item .icon) {
+        margin-right: 1ex;
+        color: var(--color-dw-scooter);
+    }
+</style>
